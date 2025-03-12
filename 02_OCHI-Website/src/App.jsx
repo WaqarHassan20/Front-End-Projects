086d39851702bc5940cea088bc5ac2cbaa5a8cb2
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-03-12 20:56:48 +0500

    Making the Animated Website OCHI Designs

--- 02_OCHI-Website/src/App.jsx
+++ 02_OCHI-Website/src/App.jsx
@@ -1,15 +1,23 @@
 import About from "./components/About";
+import Cards from "./components/Cards";
 import Eyes from "./components/Eyes";
 import Features from "./components/Features";
+import Footer from "./components/Footer";
 import LandingPage from "./components/LandingPage";
 import Marquee from "./components/Marquee";
 import Navbar from "./components/Navbar";
 import Reviews from "./components/Reviews";
+import LocomotiveScroll from "locomotive-scroll";
 
 function App() {
+  const scroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+  });
+
   return (
     <>
-      <div className="w-screen min-h-screen text-white bg-zinc-900">
+      <div id="main" className="w-screen min-h-screen text-white bg-zinc-900">
         <Navbar />
         <LandingPage />
         <Marquee />
@@ -17,6 +25,8 @@ function App() {
         <Eyes />
         <Features />
         <Reviews />
+        <Cards />
+        <Footer />
       </div>
     </>
   );
