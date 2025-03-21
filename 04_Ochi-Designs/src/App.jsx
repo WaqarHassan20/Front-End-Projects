c8a911d7be65343bc31db164ce29eeb637cdea22
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-03-22 00:33:09 +0500

    Testing the hosting on GitHub

---
+++ 04_Ochi-Designs/src/App.jsx
@@ -0,0 +1,30 @@
+import {
+  About,
+  BeforeFooter,
+  Eyes,
+  Featured,
+  Footer,
+  LandingPage,
+  Marquee,
+  Navbar,
+  Reviews,
+  StartProject
+}
+  from "./components";
+
+export default function App() {
+  return (
+    <main className="w-full h-screen ">
+      <Navbar />
+      <LandingPage />
+      <Marquee />
+      <About />
+      <Eyes />
+      <Featured />
+      <Reviews />
+      <BeforeFooter />
+      <StartProject />
+      <Footer />
+    </main>
+  )
+}
---
+++ 04_Ochi-Designs/src/assets/Images/images.js
@@ -0,0 +1,38 @@
+import OchiSvg from "./OchiSvg"
+import contentImage from "./content-image01.jpg"
+import CS_Website from "./CS_Website.png"
+import FounderImage from "./FounderImage.jpg"
+import Eyesbg from "./Eyesbg.jpg"
+import FLogo1 from "./logo001.svg"
+import FLogo2 from "./logo002.svg"
+import FLogo3 from "./logo003.png"
+import Salience_Website from "./Salience_Website.png"
+import William from "./William-Barnes.png"
+import ochiFavicon from "./ochiFavicon.png"
+import Fyde from "./Fyde.png"
+import ATG from "./ATG.png"
+import AH2 from "./ah2.png"
+import Vise from "./Vise.png"
+import Nina from './Nina.png'
+import Tomer from "./Tomer.png"
+
+
+export {
+    OchiSvg,
+    contentImage,
+    CS_Website,
+    FounderImage,
+    Eyesbg,
+    FLogo1,
+    FLogo2,
+    FLogo3,
+    Salience_Website,
+    William,
+    ochiFavicon,
+    Fyde,
+    ATG,
+    AH2,
+    Vise,
+    Nina,
+    Tomer
+}
